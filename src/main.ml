c02e05f6c83788f75e7d6a778a91036090c112ed
shinchi kogai <kogai0121@gmail.com>
2017-10-03 21:50:21 +0900

    parse JS

--- src/main.ml
+++ src/main.ml
@@ -1,14 +1,11 @@
 open Parser_flow
 
-let rec fib = function
-  | 0 -> 0
-  | 1 -> 1
-| n -> fib (n - 2) + fib (n - 1)
-
 let () =
   let filename = Some (File_key.SourceFile "my-file.js") in
   let _ = try
-    Some (jsx_pragma_expression "declare function foo(): string" filename)
+    let x = Some (jsx_pragma_expression "const foo: string = \"my-string\";" filename) in
+    print_endline "Parsed!!!";
+    x
   with
   | Parse_error.Error [] ->
     print_string "Empty";
@@ -17,5 +14,4 @@ let () =
     print_endline "Parse error!!!";
     None
   in
-      
-  print_int @@ fib 10
+  ()
